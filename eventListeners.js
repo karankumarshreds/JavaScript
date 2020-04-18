3e741f3f6b791a581f2fed54a5133ccbaf70504d
Karan Kumar <54062421+karankumarshreds@users.noreply.github.com>
2020-04-18 21:15:47 +0530

    mouseover and out

--- eventListeners.js
+++ eventListeners.js
@@ -9,3 +9,23 @@ button.onclick = function() {
   alert('You clicked again'); // <-- Only this will run
 }           // because onclick property got overwritten
 
+
+//addEventListener
+const button = document.querySelector('button');
+button.addEventListener('click', function(){
+  alert('Clicked once');
+});
+//if it's repeated
+const button = document.querySelector('button');
+button.addEventListener('click', function(){
+  alert('Clicked twice'); // <-- You get both alerts
+});
+//mouseover and mouseout
+const button = document.querySelector('button');
+button.addEventListener('mouseover', function(){
+  button.innerText = "Don't Touch";
+});
+button.addEventListener('mouseout', function(){
+  button.innerText = "Click me";
+});
+
