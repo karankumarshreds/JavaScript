e84aaabcf5e63227e10ea1c17ec91c38b6b4aa22
Karan Kumar <54062421+karankumarshreds@users.noreply.github.com>
2020-04-21 21:09:12 +0530

    Fetch.js

---
+++ HTTPRequests/Fetch.js
@@ -0,0 +1,21 @@
+//even if the HTTP error if 404 or 500, the fetch will not send
+//the error to the .catch, .then will resolve it normally changing
+//the status to 'false' instead of 'ok'.
+//.catch will only run in case of network failure.
+
+//if you want the fetch to run on failed state such as 404,
+//you need to add if(!response.ok) and throw new Error in your catch!
+fetch('https://swapi.dev/api/planets/1444444/')
+	.then((response) => {
+		if (!response.ok)
+			throw new Error(`Status Code Error: ${response.status}`);
+
+		response.json().then((data) => {
+				console.log(data);
+		});
+	})
+	//this runs only in case of network failures
+	.catch((err) => {
+		console.log('SOMETHING WENT WRONG WITH FETCH!');
+		console.log(err);
+	});
