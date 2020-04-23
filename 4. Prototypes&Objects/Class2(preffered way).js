2168a363116584cfa4f76ddfa4e6d6d9e2453623
Karan Kumar <54062421+karankumarshreds@users.noreply.github.com>
2020-04-23 12:38:45 +0530

    Class2_Preffered_Way!

---
+++ 4. Prototypes&Objects/Class2(preffered way).js
@@ -0,0 +1,13 @@
+//########### GO THROUGH PREVIOUS FILE ############//
+
+class Color {
+	//similar to init in python
+	constructor(r,g,b){	
+		this.r = r; 
+		this.g = g;
+		this.b = b;
+	}
+	rgb(){
+		return `rgb(${this.r}, ${this.g}, ${this.b})`;
+	};
+}
